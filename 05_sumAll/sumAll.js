41270aadc1f855d3556a05003b82d45be8a16b8d
Sarkoma-kisa <arti.kuryachiy@gmail.com>
2022-04-29 18:03:44 +0300

    complete all exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+  if(x < -1 || y < -1 || typeof(x) !== 'number' || typeof(y) !== 'number') {
+    return 'ERROR'
+  }
+  let sum = 0
 
+  for (i = Math.min(x, y); i <= Math.max(x, y); i++) {
+    sum += i
+  }
+
+  return sum 
 };
 
 // Do not edit below this line
