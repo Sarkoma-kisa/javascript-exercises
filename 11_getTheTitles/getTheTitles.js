525ae4c897dd8f85615d397581936e31ba0aaf9f
Sarkoma-kisa <arti.kuryachiy@gmail.com>
2022-05-05 17:03:29 +0300

    compele 11 lesson

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(item) {
+  let newArray = item.map(book => [book.title].join(''))
+  return newArray
 };
 
 // Do not edit below this line
