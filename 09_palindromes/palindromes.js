be6656f7b35ff9a7cb39234c561322eccfab29f3
Sarkoma-kisa <arti.kuryachiy@gmail.com>
2022-05-05 15:36:59 +0300

    complite lessons 08 and 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let newStr = string.split('').filter(value => value !== ' ' && value !== '!' && value !== ',' && value !== '.')
+  
+  let filteredStr = newStr.join('').toLowerCase()
 
+  let reverseStr = newStr.reverse().join('').toLowerCase()
+ 
+
+  if(filteredStr === reverseStr) {
+    return true
+  } else {
+    return false
+  }
 };
 
 // Do not edit below this line
