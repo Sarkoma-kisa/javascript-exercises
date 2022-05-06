0d03222ece0be85657950b37dfa4c35dfc98ea9e
Sarkoma-kisa <arti.kuryachiy@gmail.com>
2022-05-06 15:13:07 +0300

    complete 12 lesson

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
 
+  const date = new Date().getFullYear()
+  const oldest = arr.sort((a, b) => {
+
+    const lastPerson = (a.yearOfDeath || date) - a.yearOfBirth
+    const nextPerson = (b.yearOfDeath || date) - b.yearOfBirth
+
+    return lastPerson < nextPerson ? 1 : -1
+  })
+
+  return arr[0]
 };
 
 // Do not edit below this line
